757c8a9617feab7f7f1db999da80c812092c028c
Pawan <pawan.punnu.k@gmail.com>
2024-06-28 14:21:34 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar.js'
+import Footer from '../components/Footer.js'
+export default function Home() {
+  return (
+    <div>
+        <div> <Navbar/> </div>
+        <div> body </div>
+        <div> <Footer/> </div>
+    </div>
+  )
+}
